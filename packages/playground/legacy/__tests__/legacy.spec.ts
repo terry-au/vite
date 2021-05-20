4e014ceabce1535cbb7860e2a0dfcee3b679e685
Terry Lewis <terry1994dev@gmail.com>
2021-05-21 09:34:07 +1000

    test(playground): add test for legacy bundle name generation

--- packages/playground/legacy/__tests__/legacy.spec.ts
+++ packages/playground/legacy/__tests__/legacy.spec.ts
@@ -12,3 +12,22 @@ test('import.meta.env.LEGACY', async () => {
 test('transpiles down iterators correctly', async () => {
   expect(await page.textContent('#iterators')).toMatch('hello')
 })
+
+test('generates chunk names with hashes', async () => {
+  // we're only supporting legacy in prod,
+  // so we skip this test when isBuild is false
+  if (!isBuild) return
+
+  const scriptTags = await page.$$('//html/body/script')
+  expect(scriptTags).toHaveLength(3)
+
+  const [, polyfills, systemJSScriptSrc] = scriptTags
+
+  expect(await polyfills.getAttribute('src')).toEqual(
+    expect.stringMatching(/^\/assets\/polyfills-legacy.[a-zA-Z0-9]+.js$/)
+  )
+
+  expect(await systemJSScriptSrc.getAttribute('data-src')).toEqual(
+    expect.stringMatching(/^\/assets\/index-legacy.[a-zA-Z0-9]+.js$/)
+  )
+})
